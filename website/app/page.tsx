45463407141a4e3e4c7633f39af9524b5345d895
aaronsoetopo <aaron.soetopo@gmail.com>
2026-07-17 16:30:50 -0700

    Initialize web design

---
+++ website/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background font-sans text-accent">
+      <h1 className="text-4xl font-semibold">Repo Doctor</h1>
+    </main>
+  );
+}
